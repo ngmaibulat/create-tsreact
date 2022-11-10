e106adbfcd630da8bc553dbe2ff4daf3e0ad8ecc
ngm <ngmaibulat@gmail.com>
2022-11-10 23:26:59 +0300

    tsconfig updated

--- src/genTsConfig.ts
+++ src/genTsConfig.ts
@@ -2,17 +2,17 @@ export default function genTsConfig() {
     const tpl = `
 {
     "compilerOptions": {
-      "target": "ESNext",
+      "target": "ES2022",
       "useDefineForClassFields": true,
-      "lib": ["DOM", "DOM.Iterable", "ESNext"],
-      "allowJs": false,
+      "lib": ["DOM", "DOM.Iterable", "ES2022"],
+      "allowJs": true,
       "skipLibCheck": true,
       "esModuleInterop": false,
       "allowSyntheticDefaultImports": true,
       "strict": true,
       "forceConsistentCasingInFileNames": true,
-      "module": "ESNext",
-      "moduleResolution": "Node",
+      "module": "ES2022",
+      "moduleResolution": "NodeNext",
       "resolveJsonModule": true,
       "isolatedModules": true,
       "noEmit": true,
